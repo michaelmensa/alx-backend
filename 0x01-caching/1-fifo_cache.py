4d801784b2c76730e60575e3c1f2482e139d6fad
michaelmensa <michaelmensa@live.com>
2024-01-03 12:35:01 +0000

    task1: get method change

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -26,4 +26,4 @@ class FIFOCache(BaseCaching):
         ''' retrieve item linked to key in self.cache_data '''
         if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key)
+        return self.cache_data[key]
