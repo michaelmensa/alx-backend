151c4c524e0ecc0d4afe92e67ec0000426a80370
michaelmensa <michaelmensa@live.com>
2024-01-03 21:45:56 +0000

    task4: mru

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''
+Module 4-lru_cache: most recently used caching
+'''
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    ''' derived class BasicCache inherits from BaseCaching
+    implements MRU algo'''
+
+    def __init__(self):
+        ''' constructor method '''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        ''' method to add key, item to cached data '''
+        if key is None or item is None:
+            pass
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                discard = self.cache_data.popitem(last=True)
+                print(f'DISCARD: {discard[0]}')
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        ''' retrieve item linked to key in self.cache_data '''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
