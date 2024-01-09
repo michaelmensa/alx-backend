0d2a7dbcd2a5859960f046a1be23462bc4f72de8
michaelmensa <michaelmensa@live.com>
2024-01-09 13:45:42 +0000

    task0: basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Module 0-app.py
+'''
+from flask import FlaskI, render_template
+
+
+app = Flask(__name__)
+
+
+@app.routes('/')
+def index():
+    ''' index route '''
+    return render_template('0-index.html', title='Welcome to Holberton')
