7089f60656ba91e05caba992ecee19a6b5882e0d
michaelmensa <michaelmensa@live.com>
2024-01-09 16:35:33 +0000

    task2: get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+'''
+Module 2-app.py
+'''
+import flask_babel
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config():
+    ''' config class for configurations '''
+
+    LANGUAGES = ['en', 'fr']
+
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    ''' method to determine the best match for
+    supported languages '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+        
+
+
+@app.route('/')
+def index():
+    ''' index route '''
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
