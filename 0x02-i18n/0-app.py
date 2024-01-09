713307e764a841694f64a09749faee9ad73cd7e4
michaelmensa <michaelmensa@live.com>
2024-01-09 13:56:50 +0000

    task0: basic flask app[v]

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index() -> None:
+def index():
     ''' index route '''
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port='5000', host='0.0.0.0')
