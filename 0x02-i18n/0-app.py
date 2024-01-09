b3715439cba4f348e3684b355ca9f2a65741b0df
michaelmensa <michaelmensa@live.com>
2024-01-09 14:07:45 +0000

    task0: basic flask app[vi]

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     ''' index route '''
-    return render_template('0-index.html')
+    return render_template('0-index.html', title='Welcome to Holberton')
 
 
 if __name__ == '__main__':
