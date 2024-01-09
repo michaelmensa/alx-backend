756cd2f19652e8e041274def38ea71432fdb82ac
michaelmensa <michaelmensa@live.com>
2024-01-09 13:52:27 +0000

    task0: basic flask app[iii]

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.routes('/')
 def index():
     ''' index route '''
-    return render_template('0-index.html') 
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
