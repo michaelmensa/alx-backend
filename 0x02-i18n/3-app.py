d503c45ec018cbc8e10027d07928db5920d8e0d2
michaelmensa <michaelmensa@live.com>
2024-01-09 23:34:30 +0000

    task3:

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''
+Module 3-app.py
+'''
+from flask_babel import Babel 
+from flask import Flask, render_template, request
+
+
+class Config():
+    ''' config class for configurations '''
+
+    LANGUAGES = ['en', 'fr']
+
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    ''' method to determine the best match for
+    supported languages '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    ''' index route '''
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
