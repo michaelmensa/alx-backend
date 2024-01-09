b2ce575daa9f31bd2668288a7e36e0050a068a81
michaelmensa <michaelmensa@live.com>
2024-01-09 16:50:39 +0000

    task2: get locale from request[iv]

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,12 +20,12 @@ app.config.from_object(Config)
 
 babel = Babel(app)
 
+
 @babel.locale_selector
 def get_locale():
     ''' method to determine the best match for
     supported languages '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-        
 
 
 @app.route('/')
