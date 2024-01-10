6fb2851489c32f07f6c33d4fbde9575674a1b98f
michaelmensa <michaelmensa@live.com>
2024-01-10 00:26:13 +0000

    task4: force locale

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''
+Module 3-app.py
+'''
+from flask_babel import Babel 
+from flask import Flask, render_template, request
+
+
+class Config():
+    ''' config class for configurations '''
+
+    LANGUAGES = ['en', 'fr']
+
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    ''' method to determine the best match for
+    supported languages '''
+    requested_locale = request.args.get('locale')
+
+    if requested_locale and requested_locale in app.config['LANGUAGES']:
+        return requested_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+#babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    ''' index route '''
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
